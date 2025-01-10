e4989960a992040990e856d5c9ad1c2fcedb5cb0
socalledtheraven <cassie.dalrymple3@gmail.com>
2025-01-10 01:12:22 +0000

    cleanup + optimisation

---
+++ Files/scripts/lastChapter.js
@@ -0,0 +1,24 @@
+function main() {
+	// collects all the bookmarks and iterates through them
+	const bookmarks = document.querySelectorAll('[role="article"]');
+
+	for (const bookmark of bookmarks) {
+		let header = bookmark.querySelector(".heading");
+		let url = header.querySelector("a").href;
+
+		// checks it's not a series
+		if (url.includes("works")) {
+			let lastChapter = bookmark.querySelector("dd.chapters a").href
+
+			// checks that it's not a single-chapter dealie
+			if (lastChapter) {
+				header.querySelector("a").href = lastChapter;
+			}
+		}
+	}
+}
+
+// runs it when the page loads
+window.onload = function() {
+	main();
+};
