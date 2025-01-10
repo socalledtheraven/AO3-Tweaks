32a777ecc7f5ef2cd876ebb9528bc4d2b90cf9aa
socalledtheraven <cassie.dalrymple3@gmail.com>
2025-01-10 16:40:40 +0000

    add confirmation, update timer, add one extra affirmation

--- Files/scripts/rekudos.js
+++ Files/scripts/rekudos.js
@@ -1,12 +1,21 @@
 let messages = Array(
-    "Extra Kudos<3",
+    "Extra Kudos <3",
     "This is an extra kudos, since I've already left one. :)",
-    "I just wanted to leave another kudos<3"
+    "I just wanted to leave another kudos <3",
+    "Update kudos!"
 );
 
-let kudoButton = document.querySelector("#new_kudo")
+// CONFIG START
+const auto = false
+// CONFIG END
+
+let kudosButton = document.querySelector("#new_kudo")
 let kudosMessage = document.querySelector("#kudos_message")
-kudoButton.addEventListener("click", postComment)
+if (auto) {
+    kudosButton.addEventListener("click", postComment)
+} else {
+    kudosButton.addEventListener("click", editKudosButton)
+}
 
 function postComment() {
     setTimeout(function () {
@@ -19,5 +28,16 @@ function postComment() {
             commentBox.value = messages[random];
             commentButton.click();
         }
-    }, 0.75 * 1000);
+    }, 0.25 * 1000);
+}
+
+function editKudosButton() {
+    setTimeout(function () {
+        // checks if a kudos has already been left
+        if (kudosMessage.textContent.includes("already")) {
+            let kudosButtonText = document.querySelector("#kudo_submit")
+            kudosButtonText.value = "Rekudos?"
+            kudosButton.addEventListener("click", postComment)
+        }
+    }, 0.25 * 1000);
 }
