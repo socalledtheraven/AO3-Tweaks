2e2e36daf9ee2e39c37ac78011835d0adce5447c
socalledtheraven <cassie.dalrymple3@gmail.com>
2025-01-23 16:28:48 +0000

    refactored rekudos.js entirely, using new custom buttons i can manipulate more easily

--- Files/scripts/rekudos.js
+++ Files/scripts/rekudos.js
@@ -1,53 +1,81 @@
 // CONFIG START
+
 let messages = Array(
     "Extra Kudos <3",
     "This is an extra kudos, since I've already left one. :)",
     "I just wanted to leave another kudos <3",
-    "Update kudos!"
+    "Update kudos!",
+    "Double kudos!"
 );
 
-const AUTO = false;
 const REKUDOS_ACTIVE = true;
-const BACK_TO_CHAPTER = true;
+const AUTO = false;
 // CONFIG END
 
 function postComment() {
-    setTimeout(function () {
-        // checks if a kudos has already been left
-        if (kudosMessage.textContent.includes("already")) {
-            let random = Math.floor(Math.random() * messages.length);
-            let commentBox = document.querySelector(".comment_form");
-            let commentButton = document.querySelector("input[value='Comment']");
+    let kudosMessage = document.querySelector("#kudos_message");
 
-            commentBox.value = messages[random];
-            commentButton.click();
+    // checks if a kudos has already been left
+    if (kudosMessage.textContent.includes("already")) {
+        let random = Math.floor(Math.random() * messages.length);
+        let commentBox = document.querySelector(".comment_form");
+        let commentButton = document.querySelector("input[value='Comment']");
 
-            if (BACK_TO_CHAPTER) {
-                setTimeout(function () {
-                    // url is defined in saveAsToRead.js
-                    window.location.href = url;
-                }, (1.0 * 1000));
-            }
-        }
-    }, 0.25 * 1000);
+        commentBox.value = messages[random];
+        commentButton.click();
+    }
 }
 
-function editKudosButton() {
-    setTimeout(function () {
-        // you have to edit the value of a different element or it overwrites the whole form
-        let kudosButtonText = document.querySelector("#kudo_submit");
-        kudosButtonText.value = "Rekudos?";
-        kudosButton.addEventListener("click", postComment);
-    }, 0.25 * 1000);
+function editKudosButton(newKudosButton) {
+    // you have to edit the value of a different element or it overwrites the whole form
+    newKudosButton.textContent = "Rekudos?";
+    newKudosButton.onclick = function () {
+        postComment();
+        return false;
+    };
 }
 
-let kudosButton = document.querySelector("#new_kudo");
-let kudosMessage = document.querySelector("#kudos_message");
+function overrideButton(oldButton, newButton) {
+    if (oldButton) {
+        oldButton.insertAdjacentElement("beforebegin", newButton);
+
+        // hide it but allow it to be clicked
+        oldButton.style.display = 'none';
+    }
+}
+
 
 if (REKUDOS_ACTIVE) {
+    // jump back to the kudos button
+    let kudosButton = document.querySelector("#new_kudo");
+
+    let containerLi = document.createElement("li");
+    containerLi.style.listStyle = "none"
+    containerLi.style.display = "inline"
+    containerLi.style.paddingLeft = "0.25em"
+
+    let newKudosButton = document.createElement("a");
+    newKudosButton.textContent = "Kudos ♥";
+    newKudosButton.href = "";
+
     if (AUTO) {
-        kudosButton.addEventListener("click", postComment);
+        newKudosButton.onclick = function () {
+            let oldKudosButton = document.querySelector("#kudo_submit");
+            oldKudosButton.click();
+
+            postComment();
+            return false;
+        };
     } else {
-        kudosButton.addEventListener("click", editKudosButton);
+        newKudosButton.onclick = function () {
+            let oldKudosButton = document.querySelector("#kudo_submit");
+            oldKudosButton.click();
+
+            editKudosButton(newKudosButton);
+            return false;
+        };
     }
+
+    containerLi.appendChild(newKudosButton);
+    overrideButton(kudosButton, containerLi);
 }
